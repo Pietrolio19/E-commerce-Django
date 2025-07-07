2c60876ad791994d1cf599fe28c4aaba13b5b23a
Pietro Madioni <126822389+Pietrolio19@users.noreply.github.com>
2025-07-07 23:04:12 +0200

    Commit per script populate_db.py

--- PPM_project/settings.py
+++ PPM_project/settings.py
@@ -90,20 +90,20 @@ TEMPLATES = [
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-#DATABASES = { #database per production
-#    "default": dj_database_url.parse(
-#        DATABASE_URL,
-#       conn_max_age=600,
-#        ssl_require=True,
-#    )
-#}
-
-DATABASES = { #database per testing locale
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+DATABASES = { #database per production
+    "default": dj_database_url.parse(
+        DATABASE_URL,
+       conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
+#DATABASES = { #database per testing locale
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

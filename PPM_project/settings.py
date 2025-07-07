3510a88b6b08d313f5b6cce21e9261483d70bd89
Pietro Madioni <126822389+Pietrolio19@users.noreply.github.com>
2025-07-07 18:57:21 +0200

    Aggiunto CSRF_TRUSTED

--- PPM_project/settings.py
+++ PPM_project/settings.py
@@ -30,6 +30,11 @@ ALLOWED_HOSTS = [
     ".railway.app",
     "127.0.0.1",
     "localhost",
+    "e-commerce-django-production-20ca.up.railway.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://e-commerce-django-production-20ca.up.railway.app",
 ]
 
 

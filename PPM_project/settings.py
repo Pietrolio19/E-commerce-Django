f9d65182e2c5929f9952ef852265408435434aa2
Pietro Madioni <126822389+Pietrolio19@users.noreply.github.com>
2025-07-10 11:13:46 +0200

    Aggiustamenti

--- PPM_project/settings.py
+++ PPM_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".railway.app",
@@ -87,6 +87,7 @@ WSGI_APPLICATION = "PPM_project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 

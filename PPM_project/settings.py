21f105b5342c85bc099ae03dc4f6e851eb1dd72b
Pietro Madioni <126822389+Pietrolio19@users.noreply.github.com>
2025-07-10 11:08:47 +0200

    Aggiustamenti

--- PPM_project/settings.py
+++ PPM_project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qq%d(ascyqk@oohqoae20)&%44zy@$0+j)=_1ydc37)5rx3xkm"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".railway.app",

d48330131ec6cd6b5a21098781a0e94c185b3f62
Pietro Madioni <126822389+Pietrolio19@users.noreply.github.com>
2025-07-10 11:03:51 +0200

    Aggiustamenti

--- PPM_project/settings.py
+++ PPM_project/settings.py
@@ -88,22 +88,13 @@ WSGI_APPLICATION = "PPM_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = { #database per production
-    "default": dj_database_url.parse(
-        DATABASE_URL,
-        conn_max_age=600,
-        ssl_require=True,
-    )
+DATABASES = { #database per testing locale
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
-#DATABASES = { #database per testing locale
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
